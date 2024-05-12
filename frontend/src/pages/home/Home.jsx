f8fc1ee05373203b9df2cfc61fc0d3ae1b0a565f
Mohammad Tusher Imran Mashrafi <122280570+MashRafi-313@users.noreply.github.com>
2024-05-12 16:40:43 +0600

    sidebar for homepage created

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Sidebar from '../../components/sidebar/Sidebar'
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='flex sm:h-[450px] md:h-[450px] rounded-lg overflow-hidden
+    bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+     <Sidebar/>
+    
+    
+    </div>
   )
 }
 
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -69,3 +69,74 @@ function SignUp() {
 }
 
 export default SignUp;
+
+//starter code for signup component
+// function SignUp() {
+//   return (
+//     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+//       <div
+//         className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding 
+//     backdrop-filter backdrop-blur-1g bg-opacity-30"
+//       >
+//         <h1 className="text-3xl  font-semibold text-center text-gray-300">
+//            SignUp
+//           <span className="text-blue-500">ChatApp</span>
+//         </h1>
+//         <form>
+//           <div>
+//             <label className="label p-2">
+//               <span className="text-base label-text">Full name</span>
+//             </label>
+//             <input
+//               type="text"
+//               placeholder="Abdur Rahman"
+//               className="w-full input input-bordered h-10"
+//             />
+//           </div>
+//           <div>
+//             <label className="label">
+//               <span className="text-base label-text">Username</span>
+//             </label>
+//             <input
+//               type="text"
+//               placeholder="abdurRahman69"
+//               className="w-full input input-bordered h-10"
+//             />
+//           </div>
+//           <div>
+//             <label className="label">
+//               <span className="text-base label-text">Password</span>
+//             </label>
+//             <input
+//               type="text"
+//               placeholder="Enter Password"
+//               className="w-full input input-bordered h-10"
+//             />
+//           </div>
+//           <div>
+//             <label className="label">
+//               <span className="text-base label-text">Confirm Password</span>
+//             </label>
+//             <input
+//               type="text"
+//               placeholder="Confirm Password"
+//               className="w-full input input-bordered h-10"
+//             />
+//           </div>
+         
+//           <GenderCheckbox />
+//           <a href="#" className="text-sm hover:underline hover:text-blue-600  inline-block">
+//             Already have an account?
+//           </a>
+//           <div>
+//             <button className="btn btn-block btn-sm mt-2">Sign Up</button>
+//           </div>
+
+//           </form>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default SignUp;
+
